30170462a672ca1854801f95bee84046f2b44121
DerouillonAlice <contact@alice.sh>
2025-06-23 16:19:51 +0200

    add-feat: implement GlobalSettings custom post type for global site parameters

--- web/app/themes/zetruc-theme/app/post-types.php
+++ web/app/themes/zetruc-theme/app/post-types.php
@@ -2,7 +2,9 @@
 
 use App\PostTypes\Services;
 use App\PostTypes\CustomPost;
+use App\PostTypes\GlobalSettings;
 
 // Initialise tous les Custom Post Types
 new Services();
 new CustomPost();  // EXEMPLE : Custom Post Type universel
+new GlobalSettings(); // Paramètres globaux du site
