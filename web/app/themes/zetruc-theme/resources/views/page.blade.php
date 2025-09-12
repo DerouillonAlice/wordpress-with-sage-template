18a7dcb2f9289377e98821c58e5177992114ef90
DerouillonAlice <contact@alice.sh>
2025-09-12 15:10:55 +0200

    fix: css

--- web/app/themes/zetruc-theme/resources/views/page.blade.php
+++ web/app/themes/zetruc-theme/resources/views/page.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container mx-auto mt-16 mb-8 px-4 md:px-6">
-    <h1 class="h2  text-center">@php(the_title())</h1>
+    <h1 class="text-4xl text-center">@php(the_title())</h1>
 </div>
 
 <div class="container mx-auto my-16 px-4 md:px-6">
