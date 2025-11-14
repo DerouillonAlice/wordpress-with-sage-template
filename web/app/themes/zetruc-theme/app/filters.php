9c2f7efa375b968759573415ac749bde750bf7b0
Alice Derouillon <alice.derouillon@zetruc.fr>
2025-11-14 15:19:03 +0000

    fix: global style inline css

--- web/app/themes/zetruc-theme/app/filters.php
+++ web/app/themes/zetruc-theme/app/filters.php
@@ -14,3 +14,34 @@ namespace App;
 add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'sage'));
 });
+
+/**
+ * Désactiver les styles globaux WordPress injectés automatiquement.
+ * Ils seront réenregistrés dans un layer CSS personnalisé.
+ *
+ * @return void
+ */
+add_action('wp_enqueue_scripts', function () {
+    wp_dequeue_style('global-styles');
+    wp_deregister_style('global-styles');
+}, 10);
+
+/**
+ * Réenregistrer les styles globaux WordPress dans un layer CSS.
+ * Cela permet à Tailwind de reprendre correctement la priorité.
+ *
+ * @return void
+ */
+add_action('wp_enqueue_scripts', function () {
+    $stylesheet = wp_get_global_stylesheet();
+    
+    if (empty($stylesheet)) {
+        return;
+    }
+
+    wp_register_style('global-styles', false);
+    wp_add_inline_style('global-styles', "@layer global-block-styles { $stylesheet }");
+    wp_enqueue_style('global-styles');
+
+    wp_add_global_styles_for_blocks();
+}, 11);
